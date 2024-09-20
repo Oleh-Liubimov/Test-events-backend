77e3943c194ab5264ca8072ec6f20ee9c023bbb3
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-09-20 11:37:28 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://test-front-events.vercel.app',
+    origin: ['https://test-front-events.vercel.app', 'http://localhost:3000'],
     methods: 'GET,POST',
     credentials: true,
   });
